9968674617137876d668c414940cac9d4c31f0a8
4drea4 <bakerand@outlook.com>
2025-12-26 15:55:09 -0600

    styling

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -14,17 +14,23 @@ export default function Home() {
         return <p>It's loading..</p>
     }
     return(
-        <div>
+        <div className="page">
             <h1 className="title">Recipe Discovery</h1>
             <ul className="unorderedList">
                  {data.categories.map((category) => (
                 <li className="link" key= {category.idCategory}>
-                 <Link to={`/category/${category.strCategory}`}>{category.strCategory}</Link>
+                 <Link to={`/category/${category.strCategory}`}>
+                 <img
+                 className="card"
+                 src={category.strCategoryThumb}/>
+                 <div className="card">
+                    <h2 className="cardTitle">{category.strCategory}</h2>
+                    <p className="cardText">{category.strCategoryDescription?.slice(0,110)}</p>
+                 </div>
+                
+                 </Link>
                 </li>
-          
         ))}
-
-    
             </ul>
         </div>
     )
