9241e74771fc557ad9296c526332191352ec14c3
4drea4 <bakerand@outlook.com>
2025-12-26 12:55:40 -0600

    working on favorites and troubleshooting recipe details

--- src/pages/favorite/Favorites.tsx
+++ src/pages/favorite/Favorites.tsx
@@ -1,8 +1,51 @@
+import { Link } from "react-router-dom";
+import { useFavorites } from "../../context/FavoritesContext";
+import { useFetch } from "../../hooks/useFetch";
+import type { RecipeDetailResponse } from "../../types";
+
+function FavoriteRecipe({recipeId}: {recipeId:string}){
+    const {removeFavorite} = useFavorites();
+
+const url = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${recipeId}`;
+
+const {data, error} = useFetch<RecipeDetailResponse>(url);
+
+if(error) return <li> Error loading {recipeId} from favorites</li>
+if(!data) return <li>Loading {recipeId} from favorites</li>
+
+const recipe = data.meals?.[0];
+if(!recipe) return <li>I cant find this recipde {recipeId}</li>
+
+return(
+    <li>
+        <Link to={`/recipe/${recipeId}`}>{recipe.strMeal}</Link>
+        <button onClick={()=> removeFavorite(recipeId)}>Remove</button>
+  
+    </li>
+
+)
+}
 export default function FavoritesPage(){
-    return (
+    const {favoriteIds} = useFavorites();
+    return ( 
         <div>
+            <p>
+                <Link to ='/'>Back to Categories</Link>
+            </p>
+
             <h1>Favorites</h1>
-            <p>Favorite recipes</p>
+            {favoriteIds.length === 0 ? (
+                <p>No favorites yet</p>
+            ): (
+                <ul>
+                    {favoriteIds.map((id) => (
+                        <FavoriteRecipe key={id} recipeId={id}/>
+                    )
+                )}
+                </ul>
+            )
+            }
+           
         </div>
     )
 }
--- src/pages/recipe/RecipeDetail.tsx
+++ src/pages/recipe/RecipeDetail.tsx
@@ -1,9 +1,11 @@
 import { useParams , Link} from "react-router-dom";
 import { useFetch } from "../../hooks/useFetch";
 import type { RecipeDetailResponse } from "../../types";
+import { useFavorites } from "../../context/FavoritesContext";
 
 export default function RecipeDetailPage(){
     const {recipeId} = useParams();
+    const {addFavorite, removeFavorite, isFavorite} =useFavorites();
      
     if (!recipeId){
         return <p> The recipe id is missing</p>
@@ -21,20 +23,37 @@ export default function RecipeDetailPage(){
     if (!recipe) {
         return <p> Cant seem to find this recipe</p>
     }
+    const saved = isFavorite(recipeId);
+    function handleFavoriteClick(){
+        if(saved) {
+            removeFavorite(recipeId);
+        } else {
+            addFavorite(recipeId);
+        }
+    }
+    
     return (
         <div>
             <p>
-                <Link to='/'>Go back to Categories</Link>
+                <Link to='/'>Go back to Categories</Link>{""}|
+                <Link to='/favorites'>Favorites</Link>
             </p>
+
             <h1> {recipe.strMeal}</h1>
+
+            <button onClick={handleFavoriteClick}>
+                {saved ? 'Remove from your Favorites' : 'Add to favorites'}
+            </button>
+
             {recipe.strMealThumb && (
                 <img
                 src={recipe.strMealThumb}
+                alt={recipe.strMeal}
                 />
             )}
             <h3>Instructions</h3>
             <p>{recipe.strInstructions}</p>
             <p>View recipe id: {recipeId} </p>
         </div>
-    )
+    );
 }
