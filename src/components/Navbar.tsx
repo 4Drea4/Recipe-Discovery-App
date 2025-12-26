a95fcb47cbcd8855d5763dafb5aa9d855e11f0ce
4drea4 <bakerand@outlook.com>
2025-12-26 14:48:41 -0600

    found the error i was using the wrong syntax

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,14 +1,15 @@
 import { useState } from "react";
-
+import { useNavigate } from "react-router-dom";
 
 export default function SearchBar(){
     const [query, setQuery] = useState("");
-    
+    const navigate = useNavigate();
 
     function handleSearch(){
         const searchQuery = query.trim();
         if (!searchQuery) return;
        
+        navigate(`/search?query=${encodeURIComponent(searchQuery)}`);
     }
 
     return (
